bca5d690fece13964f6c3ce4f6a8a80a462fab2c
JoeseaLea <354046335@qq.com>
2019-12-05 21:21:16 +0800

    单元测试

--- src/test/java/com/stocksmarket/service/StockServiceTest.java
+++ src/test/java/com/stocksmarket/service/StockServiceTest.java
@@ -1,5 +1,39 @@
-import static org.junit.Assert.*;
+package com.stocksmarket.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class StockServiceTest {
+    @Autowired
+    private StockService stockService;
 
+    @Test
+    public void insertOrUpdateStock() {
+    }
+
+    @Test
+    public void getAllStockCode() {
+        List<String> list = stockService.getAllStockCode();
+        System.out.println(list.size());
+    }
+
+    @Test
+    public void resetLastHisDataDownFlag() {
+    }
+
+    @Test
+    public void getAllNotDownHisDataStockVo() {
+    }
+
+    @Test
+    public void getAllDownHisDataStockVo() {
+    }
 }
